ec5583ab0d1a6aa065daa475f511cb6d3bc9888a
Uddalak Mukhopadhyay <uddalakmukhopadhyay@gmail.com>
2025-11-04 18:34:37 +0530

    cors allowed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,12 @@
 import express from "express";
+import cors from "cors";
 import userRoutes from "./routes/user.routes.js";
 
 const app = express();
+
+app.use(cors({
+    origin: "*"
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
